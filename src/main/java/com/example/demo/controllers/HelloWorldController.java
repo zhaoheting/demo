b1b4e5e17645aafd8e5a6c4ca818a39a781eee98
zhaohetingopenjaw <m15040605612@163.com>
2018-12-25 14:30:19 +0800

    test stash

--- src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -1,5 +1,14 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloWorldController {
+	
+	@RequestMapping()
+	public String helloWorld() {
+		 return "";
+	}
 
 }
