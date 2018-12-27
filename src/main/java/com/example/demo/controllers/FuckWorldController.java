afa060ee4ade625d3f0e245a959d8b3eba3b0437
zhaoheting <492995407@qq.com>
2018-12-27 20:13:00 +0800

    commit second

--- src/main/java/com/example/demo/controllers/FuckWorldController.java
+++ src/main/java/com/example/demo/controllers/FuckWorldController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class FuckWorldController {
 
+	@RequestMapping
+	public String fuckWorld() {
+		return "";
+	}
 }
