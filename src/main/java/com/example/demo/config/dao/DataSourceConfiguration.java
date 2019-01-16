c7a46fe1858ff3e826a7cf8cfe27459712476ced
zhaoheting <heting.zhao@openjawtech.com>
2019-01-16 13:30:11 +0800

    submit configuration file

--- src/main/java/com/example/demo/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/example/demo/config/dao/DataSourceConfiguration.java
@@ -1,14 +1,36 @@
 package com.example.demo.config.dao;
 
+import java.beans.PropertyVetoException;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+@Configuration
+//配置mybatis mapper的扫描路径
+@MapperScan("com.example.demo.dao")
 public class DataSourceConfiguration {
 
+	@Value("${jdbc.driver}")
+	private String jdbcDriver;
+	@Value("${jdbc.url}")
+	private String jdbcUrl;
+	@Value("${jdbc.username}")
+	private String user;
+	@Value("${jdbc.password}")
+	private String password;
+
 	@Bean(name="dataSource")
-	public ComboPooledDataSource createDataSource() {
+	public ComboPooledDataSource createDataSource() throws PropertyVetoException {
 		ComboPooledDataSource dataSource =new ComboPooledDataSource();
+		dataSource.setDriverClass(jdbcDriver);
+		dataSource.setJdbcUrl(jdbcUrl);
+		dataSource.setUser(user);
+		dataSource.setPassword(password);
+		dataSource.setAutoCommitOnClose(false);
 		return dataSource;
 	}
 }
